b2dbe6bc6628c5c2cdcdd33fb50e336f148bc582
wumingjie <2907829008@qq.com>
2023-04-24 18:27:42 +0800

    init

---
+++ src/main/java/com/suqb/www/MysqlDocGeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.suqb.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 29078
+ */
+@SpringBootApplication
+public class MysqlDocGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlDocGeneratorApplication.class, args);
+    }
+
+}
